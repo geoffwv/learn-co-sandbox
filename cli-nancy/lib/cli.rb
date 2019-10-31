2bd872e6040159d4a52e0aedf338871caab3d2c9
Geoff Elliott <tinywv@gmail.com>
2019-10-31 01:27:18 +0000

    working scraper

---
+++ cli-nancy/lib/cli.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+require 'open-uri'
+
+def run
+  puts "Welcome to IMDB Openings!"
+  puts "These are the movies opening this week:"
+  puts " "
+  puts ": ======================= :"
+  puts " "
+
+  html = open("https://www.imdb.com/")
+  doc = Nokogiri::HTML(html)
+  movies = []
+  doc.css("div.rhs-row").each do |mov|
+    title = mov.css(".title").text.strip
+    movies << title
+  end
+
+  movies.each.with_index(1) do |title, index|
+    puts "#{index}. #{title}"
+  end
+  
+end
