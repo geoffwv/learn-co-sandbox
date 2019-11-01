de94fd71858b7867aac586a37b45a30e6199d5ce
Geoff Elliott <tinywv@gmail.com>
2019-11-01 21:52:23 +0000

    final file structure

--- sg_gem_movies/lib/sg_gem_movies.rb
+++
@@ -1,8 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-
-require_relative "./sg_gem_movies/cli"
-require_relative "./sg_gem_movies/movie"
-require_relative "./sg_gem_movies/scraper"
-require_relative "./sg_gem_movies/version"
